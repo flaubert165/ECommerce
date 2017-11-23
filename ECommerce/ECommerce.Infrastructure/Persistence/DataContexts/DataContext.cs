092138d430a647ffeed9dfecd7893cbcfc32fdf6
italosantana <flaubert165@gmail.com>
2017-11-23 16:55:27 -0300

    Ajustes DataContext

--- ECommerce/ECommerce.Infrastructure/Persistence/DataContexts/DataContext.cs
+++ ECommerce/ECommerce.Infrastructure/Persistence/DataContexts/DataContext.cs
@@ -10,6 +10,10 @@ namespace ECommerce.Infrastructure.Data
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
 
     }
 }
